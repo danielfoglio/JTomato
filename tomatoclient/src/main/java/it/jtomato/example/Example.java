9a67840295cd9d4aef51ec61343eacfdd5240cb5
geeordanoh <tambug@gmail.com>
2013-03-18 22:13:52 +0100

    refined example

--- tomatoclient/src/main/java/it/jtomato/example/Example.java
+++ tomatoclient/src/main/java/it/jtomato/example/Example.java
@@ -15,27 +15,27 @@ import java.util.List;
  **/
 public class Example {
 
-	
 	public static void main(String[] args) {
-		//Creating a JTomato instance, you need a vali API key.
-		JTomato jtomato = new JTomato("put your key here");
-		
-		//Creating a list to store the result
+		// Creating a JTomato instance, you need a vali API key.
+		JTomato jtomato = new JTomato("put your api key here");
+
+		// Creating a list to store the result
 		List<Movie> movies = new ArrayList<Movie>();
-		
-		//Searching a movie, page 1 
+
+		// Searching a movie, page 1
 		int total = jtomato.searchMovie("spider man", movies, 1);
-		System.out.println("Found "+total+" results");
-		System.out.println("First result: "+ movies.get(0));
-		
-		//Obtaning additional info
+		System.out.println("Found " + total + " results");
+		System.out.println("First result: " + movies.get(0));
+
+		// Obtaining additional info
 		Movie movie = jtomato.getMovieAdditionalInfo(movies.get(0));
-		//Printing the movie genre
+		// Printing the movie genre
 		System.out.println(movie.genres.get(0));
-		
-		//Getting similar movies
+
+		// Getting similar movies
 		List<Movie> similarMovies = jtomato.getSimilarMovies(movie, "us", 5);
-		
+		System.out.println("A similar movie: " + similarMovies.get(0));
+
 	}
 
 }
